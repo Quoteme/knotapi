26b9b8538e92b39e31f050f162ab7526c46565c9
Luca Leon Happel <lucahappel99@gmx.de>
2026-02-09 15:25:08 +0100

    add: time estimate and csv/json

--- generate.sh
+++ generate.sh
@@ -3,18 +3,22 @@
 wget https://raw.githubusercontent.com/knotinfo-org/assets/main/knotinfo_3d-coordinates.csv.tar.gz
 tar -xzf knotinfo_3d-coordinates.csv.tar.gz
 infile="./3d-coordinates.csv"
+lines=$(wc -l <"$infile")
+current=0
 
 html="<!DOCTYPE html><html><head><meta charset="utf-8"><title>Knotapi</title></head><body><ul>"
 
 while IFS=',' read -r col1 col2 col3 col4; do
+	echo "Processing $col1 ($((++current))/$lines)"
 	# skip empty lines
 	[ -z "$col1" ] && continue
 
-	html="$html<li><a href='$col1.csv'>$col1</a></li>"
+	html="$html<li><a href='$col1.json'>$col1.json</a> <a href='$col1.csv'>$col1.csv</a><</li>"
 
 	# create file named after first column
 	# and write the third column as content
-	printf '%s\n' "$col4" >"$col1.csv"
+	printf '%s\n' "${col4//;/,}" >"$col1.json"
+	printf '%s\n' "${col4//;/,}" | tr -d '[]' >"$col1.csv"
 done <"$infile"
 
 html="$html</ul></body></html>"
